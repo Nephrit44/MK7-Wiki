fa75b6c2eed951ad8444faa9f096eac793dba3ea
Nephrit44 <nephrit44@yandex.ru>
2025-10-09 21:42:57 +0300

    Сделаны функции загрузки данных

---
+++ js/contents.js
@@ -0,0 +1,33 @@
+const contents = [
+    {
+        recID: 1,
+        titleName: "М ОС",
+    },
+    {
+        recID: 2,
+        titleName: "1С Предприятие",
+    }
+];
+
+const navigation = [
+    {
+        valueID: 1,
+        subtitleName: "Операционная система М ОС 1",
+        dataLink: "db0001",
+    },
+        {
+        valueID: 1,
+        subtitleName: "Операционная система М ОС 2",
+        dataLink: "db0001",
+    },
+        {
+        valueID: 1,
+        subtitleName: "Операционная система М ОС 3",
+        dataLink: "db0001",
+    },
+    {
+        valueID: 2,
+        subtitleName: "Настройка учётных записей 1С",
+        dataLink: "db0001",
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,28 +1,68 @@
+const mainContent = document.querySelector('.main-content'); //Сюда вставляем все, что генерируется
+const getDefault_mainContent = document.querySelector("#default_main-content"); //Макет заголовков
+
 const getTemplate_ROW = document.querySelector('#defaultRow');
 const getList = document.querySelector('.list');
+const defaultCounter = 1; //Число счётчика с которого начинается отсчёт
 
-loadDB(db0001, 1);
+const getLi = document.querySelector("#li"); //Поиск макета элементов li
 
-//Загрузка списка базы данных
-function loadDB (dataBase, counter){
-dataBase.forEach((element) => {
+loadContents(contents, defaultCounter); //Загрузка оглавлений
+
+
+/*
+    На вход имя массива и число для счётчка.
+    1. Клонируется элемент с заголовком и UL для подсписка
+    2. Ищется в клонированном элементе заголовок и вводится в него содержимое из базы
+    3. Вызывается функция фильтр, которая ищет всё, что связано с текущим разделом.
+    4. Добавляется на экран
+*/
+function loadContents(dataBase, counter) {
+    dataBase.forEach((element) => {
+        let newElement = getDefault_mainContent.content.cloneNode(true); //Клонируем
+        newElement.querySelector('.main-content__title').textContent = element.titleName;
+        filterNavigation(navigation, element.recID, newElement.querySelector('.main-content__list'));
+        mainContent.append(newElement);
+    });
+};
+
+/*
+Функция фильтр: 
+1. На вход получает, базу сподразделами
+2. значение с котором быдет сравнивать. Берётся из 
+элемент в который вставит найденые данные.
+*/
+function filterNavigation(navigation, valueID, list) {
+    const result = navigation.filter(nav => nav.valueID == valueID);
+    result.forEach((element) => {
+        let newElement = getLi.content.cloneNode(true); //Клонируем
+
+        newElement.querySelector(".main-content__item").textContent = element.subtitleName;
+        list.append(newElement);
+    });
+};
+
+
+//Загрузка инструкции
+function loadInstruction(dataBase, counter) {
+    dataBase.forEach((element) => {
         let newElement = getTemplate_ROW.content.cloneNode(true); //Клонируем
         const templateIMG = newElement.querySelector('.block-img');
         const blockCode = newElement.querySelector('.block-code');
-        
+
         newElement.querySelector('.counter').textContent = counter;
         newElement.querySelector('.block-content').innerText = element.textContent;
         if (element.blockCode == "") {
             blockCode.className = "visible";
-        }else{
+        } else {
             blockCode.textContent = element.blockCode;
         }
-        templateIMG.src = element.blockImg; 
-        if(element.typeImg != ""){
-         templateIMG.classList.add(element.typeImg);   
+        templateIMG.src = element.blockImg;
+        if (element.typeImg != "") {
+            templateIMG.classList.add(element.typeImg);
         }
         templateIMG.setAttribute('alt', element.altImg);
-        counter ++;
+        counter++;
         getList.append(newElement);
     });
-}
+};
